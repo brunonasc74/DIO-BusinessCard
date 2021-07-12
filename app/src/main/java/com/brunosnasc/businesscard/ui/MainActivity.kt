df529e5b5503d63de1dab59fd189790f50112c4a
BrunoNasc74 <bruno.hpn74@gmail.com>
2021-07-12 19:14:36 -0300

    Created ViewModel

--- app/src/main/java/com/brunosnasc/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/brunosnasc/businesscard/ui/MainActivity.kt
@@ -2,16 +2,25 @@ package com.brunosnasc.businesscard.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.brunosnasc.businesscard.App
 import com.brunosnasc.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -19,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCards ->
+            adapter.submitList(businessCards)
         }
     }
 }
---
+++ app/src/main/java/com/brunosnasc/businesscard/ui/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.brunosnasc.businesscard.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.brunosnasc.businesscard.data.BusinessCard
+import com.brunosnasc.businesscard.data.BusinessCardRepository
+import java.lang.IllegalArgumentException
+
+class MainViewModel(private val businessCardRepository: BusinessCardRepository) : ViewModel() {
+
+    fun insert(businessCard: BusinessCard) {
+        businessCardRepository.insert(businessCard)
+    }
+
+    fun getAll(): LiveData<List<BusinessCard>> {
+        return businessCardRepository.getAll()
+    }
+}
+
+class MainViewModelFactory(private val repository: BusinessCardRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
