c2a5b145edf07ceb1e52a68775be41b0a88183d8
BrunoNasc74 <bruno.hpn74@gmail.com>
2021-07-12 11:50:24 -0300

    Added dependencies and changed ContentView

---
+++ app/src/main/java/com/brunosnasc/businesscard/App.kt
@@ -0,0 +1,6 @@
+package com.brunosnasc.businesscard
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/brunosnasc/businesscard/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.brunosnasc.businesscard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.brunosnasc.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
